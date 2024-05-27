605672efbbe18bfb48132d308431985371393795
Amadesp <137555323+Amadesp@users.noreply.github.com>
2024-05-27 15:51:49 +0200

    Atualizar o settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-Mz'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Maputo'
 
 USE_I18N = True
 
